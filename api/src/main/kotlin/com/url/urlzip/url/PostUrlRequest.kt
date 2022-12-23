2b11b55261e0d83401edadb658648b6bb46d83cd
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-12-24 00:59:27 +0900

    [add-save-url-service] feature: add validator

--- api/src/main/kotlin/com/url/urlzip/url/PostUrlRequest.kt
+++ api/src/main/kotlin/com/url/urlzip/url/PostUrlRequest.kt
@@ -1,5 +1,8 @@
 package com.url.urlzip.url
 
+import javax.validation.constraints.NotBlank
+
 data class PostUrlRequest(
-    val url: String
+    @field:NotBlank(message = "url은 null일 수 없습니다.")
+    val url: String?
 )
