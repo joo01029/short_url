a26f38d3e67eb06a2676ed12ce271e3eebe98cbd
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-12-24 02:20:24 +0900

    [add-save-url-service] feature: add base62 dependency

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,9 @@
+val base62Version: String by project
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.seruco.encoding:base62:$base62Version")
     implementation(project(":db"))
 }
 
