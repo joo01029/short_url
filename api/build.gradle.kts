557d8dc0ece1f8011dd1b0ed36fbb579faf1525d
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-12-23 23:59:30 +0900

    [add-save-url-service] feature:add dependency db module to api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,7 +1,7 @@
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-test")
+    implementation(project(":db"))
 }
 
 tasks.getByName("bootJar") {
