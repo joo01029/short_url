db5e0440241eb49f7259364623f27a72aa0fd4b4
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-12-28 12:49:13 +0900

    [change-db-module-jpa] update: build.gradle.kts dependency jpa

--- db/build.gradle.kts
+++ db/build.gradle.kts
@@ -1,5 +1,12 @@
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
 dependencies {
-    api("org.springframework.boot:spring-boot-starter-data-mongodb")
+    api("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java")
 }
 
 tasks.getByName("bootJar") {
